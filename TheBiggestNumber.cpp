a85c3a099a0cd4f1ce37165123e637fa98d7bafc
Johnny <johnnyalbadeen10@gmail.com>
2020-06-19 13:02:02 +0200

    finished!

--- TheBiggestNumber.cpp
+++ TheBiggestNumber.cpp
@@ -5,19 +5,26 @@ using namespace std;
 	int a, b, c;
 	cout<<"Enter 3 numbers respectively: ";
 	cin>>a>>b>>c;
-	if(a>b && a>c){
+	/*while(!cin>>a>>b>>c){
+		cout<<"You did not enter 3 valid numbers!";
+		cin.ignore();
+	}*/
+	while(cin.fail())
+{
+    cin.clear();
+    cout << "You didn't enter 3 valid numbers!";
+    return 0;
+}
+	
+	if(a>=b && a>=c){
 		cout<<"The biggest number is: "<<a;
 		return 0;
-	}else if(b>a && b>c){
+	}else if(b>a && b>=c){
 		cout<<"The biggest number is: "<<b;
 		return 0;
 	}else if(c>a && c>b){
 		cout<<"The biggest number is: "<<c;
 		return 0;
-	}if(a==b && a==c && b==c){
-		cout<<"The numbers are equal";
-		return 0;
-	}if(a==b || a==c || b==c){
-		cout<<"you entered the same number 2 times";
 	}
- }  
+}
+ 
